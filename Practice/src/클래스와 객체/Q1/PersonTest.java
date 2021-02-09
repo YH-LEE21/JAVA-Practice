5add490146e9af724f4667fe2758771884256b3c
YOHAN <73163074+YH-LEE21@users.noreply.github.com>
2021-02-09 21:28:05 +0900

    Create PersonTest.java

---
+++ Practice/src/클래스와 객체/Q1/PersonTest.java
@@ -0,0 +1,30 @@
+package Q3;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		Person James = new Person();
+		James.setAge(40);
+		James.setName("James");
+		James.setMarried(true);
+		James.setNumberOfChildren(3);
+  		
+		System.out.println("나이:"+James.getAge());
+		System.out.println("이름:"+James.getName());
+		System.out.println("결혼여부:"+James.getMarried());
+		System.out.println("자녀 수:"+James.getNumberOfChildren());
+	
+    Person Yohan = new Person();
+    Yohan.age = 23;
+    Yohan.name ="이요한";
+    Yohan.married = false;
+    Yohan.NumberOfChildren = 0;
+    
+    System.out.println("나이:"+Yohan.getAge());
+		System.out.println("이름:"+Yohan.getName());
+		System.out.println("결혼여부:"+Yohan.getMarried());
+		System.out.println("자녀 수:"+Yohan.getNumberOfChildren());
+	
+	}
+
+}
