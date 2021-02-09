152193cba1e076ef71a2830fcb911a85201eb5f8
YOHAN <73163074+YH-LEE21@users.noreply.github.com>
2021-02-09 23:58:37 +0900

    Create Order.java

---
+++ Practice/src/클래스와 객체/Q2/Order.java
@@ -0,0 +1,50 @@
+package Q2;
+
+public class Order {
+	public String order_num;
+	public String order_id;
+	public String order_date;
+	public String name;
+	public String order_presentNum;
+	public String address;
+	
+	public Order() {}
+	public	String getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(String order_num) {
+		this.order_num = order_num;
+	}
+	public String getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(String order_id) {
+		this.order_id = order_id;
+	}
+	public String getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(String order_date) {
+		this.order_date = order_date;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOrder_prsentNum() {
+		return order_presentNum;
+	}
+	public void setOrder_prsentNum(String order_prsentNum) {
+		this.order_presentNum = order_prsentNum;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
