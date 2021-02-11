474b40028133fbe201ce0efcf966cf6150402ebf
YOHAN <73163074+YH-LEE21@users.noreply.github.com>
2021-02-11 16:26:43 +0900

    Create MyDateTest.java

---
+++ Practice/src/클래스와 객체/Q3/MyDateTest.java
@@ -0,0 +1,12 @@
+package Q3;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate Today = new MyDate(31, 3, 2021);
+		
+		System.out.println(Today.isValid());
+	}
+
+}
